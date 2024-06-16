ff9746beb4da28fe31b74c92343f981184637d24
Dave Murphy <davem@devkitpro.org>
2024-06-16 09:44:47 +0100

    make read/write posix compliant

--- newlib/libc/include/sys/config.h
+++ newlib/libc/include/sys/config.h
@@ -25,6 +25,11 @@
 #define __DYNAMIC_REENT__
 #endif
 
+#ifdef __DEVKITPRO__
+#define _READ_WRITE_RETURN_TYPE _ssize_t
+#define _READ_WRITE_BUFSIZE_TYPE size_t
+#endif
+
 #ifdef __AMDGCN__
 #define __DYNAMIC_REENT__
 #endif
